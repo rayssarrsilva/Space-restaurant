d316133ca3782a3c6950cf5febc0b545c17c063d
Rayssa <rayssarrsilva1@gmail.com>
2026-07-08 17:35:22 -0300

    Import the css files to the entry js

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import './styles/about.css'
+import './styles/header.css'
+import './styles/home.css'
+import './styles/menu.css'
--- webpack.config.js
+++ webpack.config.js
@@ -15,4 +15,12 @@ export default {
         template: "./src/index.html",
     }),
   ],
+  module: {
+    rules: [
+        {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+        },
+    ],
+  },
 };

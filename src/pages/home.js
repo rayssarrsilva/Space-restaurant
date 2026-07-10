b9476f364fb2dea2900b261a45f16b57e3d6c16c
Rayssa <rayssarrsilva1@gmail.com>
2026-07-10 18:20:41 -0300

    Clean the home js file

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,4 +1,4 @@
-// src/home.js
+// home.js
 export default function loadHome() {
   const container = document.createElement("div");
   container.classList.add("home");
@@ -6,19 +6,6 @@ export default function loadHome() {
   const title = document.createElement("h2");
   title.textContent = "Welcome to Space Bakery";
 
-  const backGround = document.createElement("img");
-  backGround.src = "./images/space.jpg";
-
-  const description = document.createElement("p");
-  description.textContent = "Aqui você encontra pratos deliciosos e um ambiente acolhedor.";
-
-  const image = document.createElement("img");
-  image.src = "https://via.placeholder.com/300x200"; // pode trocar por uma imagem real
-  image.alt = "Foto do restaurante";
-
   container.appendChild(title);
-  container.appendChild(description);
-  container.appendChild(backGround);
-
   return container;
 }

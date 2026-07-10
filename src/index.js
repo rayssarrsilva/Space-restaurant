4fdd19fedb54884707d055fc63aa9360a4c50004
Rayssa <rayssarrsilva1@gmail.com>
2026-07-10 18:29:16 -0300

    Add zero space around the home image

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import loadHome from "./pages/home.js";
 import "./styles/home.css";
+import "./styles/header.css";
 
 function init(){
     const content = document.getElementById("content");
---
+++ src/pages/header.js
@@ -0,0 +1,8 @@
+export default function loadHeader() {
+    const header = document.getElementById("header");
+    const nav = document.getElementById("nav");
+    const menuButton = document.getElementById("menu");
+    const reservationButton = document.getElementById("reservation");
+    const home = document.getElementById("home");
+    return header;
+}
